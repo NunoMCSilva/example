8af300bbc3222225973e3ac5739b76fec07840ec
Nuno Miguel Casteloa da Silva <NunoMCSilva@gmail.com>
2019-08-08 03:41:18 +0100

    Add basic code

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+def read(fname: str) -> str:
+    with open(fname) as fp:
+        return fp.read()
+
+
+setuptools.setup(
+    name="example-pkg-blackcat",
+    version="0.0.1",
+    author="Nuno Miguel Casteloa da Silva",
+    author_email="NunoMCSilva@gmail.com",
+    description="A small example package",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/NunoMCSilva/example",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
